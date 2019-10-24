ef00e52bf94acd54bdd41b48d44395eaa405427c
smilerac <smilerac15@gmail.com>
2019-10-25 01:39:54 +0545

    Income CRUD complete, however update isn't working properly

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,9 @@ var iexpenseinfo = require('../models/Immediateexpensemodel')
 var texpenseinfo = require('../models/trueexpensemodel')
 var pexpenseinfo = require('../models/pleasureexpensemodel')
 var mygoals = require('../models/goalsmodel')
+// var async = require('async');
+// var _ = require('lodash');
+
 
 // /* GET home page. */
 // router.get('/', function(req, res, next) {
@@ -66,6 +69,48 @@ router.post('/addpocket', function(req, res, next) {//all data is in req.body
   }).catch(err=> console.log(err+"could not save....."))
 });
 
+router.get('/income/:incomeId', function(req, res, next) {
+  // var movieId = req.params.id;
+  // Movies.findOne({_id: req.params.movieId}, function(err, movie){
+  //   console.log('moviesssssss', movie)
+  // }
+  //console.log(req.paramsmovieId)
+  
+  incomeinfo.findOne({ _id : req.params.incomeId}, function(err, oneincome){
+    // var movie = item => item._id === movieId
+    res.render('eachincome',{incomes:oneincome}); //sends 'movies' data to 'viewOne' view
+  })
+})
+
+router.get('/edit/:incomeId', function(req, res, next) {
+    // var movieId = req.params.id;
+    // Movies.findOne({_id: req.params.movieId}, function(err, movie){
+    //   console.log('moviesssssss', movie)
+    // }
+    //console.log(req.paramsmovieId)
+    
+    incomeinfo.findOne({ _id : req.params.incomeId}, function(err, oneincome){
+      // var movie = item => item._id === movieId
+      res.render('form',{incomes:oneincome}); //sends 'movies' data to 'viewOne' view
+    })
+  })
+  
+  router.post('/saveincome/:incomeId', function(req, res, next) {//all data is in req.body
+    console.log(req.body) //shows value in terminal
+    incomeinfo.findOneAndUpdate({ _id : req.body._id}, {$set: req.body}, function(err, movie){
+     // console.log(movieId+"this is iddddd")
+      res.redirect("/income")
+    })
+  });
+
+  router.get('/remove/:incomeId', function(req, res, next){
+    incomeinfo.deleteOne({ _id : req.params.incomeId}, function(err, incomes){
+     // console.log(movieId + 'heyyyy')
+      // var movie = item => item._id === movieId
+     res.redirect("/income")
+      // res.delete(onemovie); //sends 'movies' data to 'viewOne' view
+    })
+  })
 // router.get('/test', function(req, res, next) {
 //   res.render('incomecopy');
 // });
@@ -96,10 +141,17 @@ router.get('/expense', function(req, res, next) {
           function(callback) {
               db.collection('iexpenses').find({}).toArray(function(err, iexpenses) {
                   if (err) return callback(err);
-                  locals.iexpenses = iespenses;
+                  locals.iexpenses = iexpenses;
                   callback();
               });
           },
+          function(callback) {
+            db.collection('pexpenses').find({}).toArray(function(err, pexpenses) {
+                if (err) return callback(err);
+                locals.pexpenses = pexpenses;
+                callback();
+            });
+        },
           // Load colors
           function(callback) {
               db.collection('texpenses').find({}).toArray(function(err, texpenses) {
@@ -107,16 +159,18 @@ router.get('/expense', function(req, res, next) {
                   locals.texpenses = texpenses;
                   callback();
               });
-          
-      
+            }
+            ]
 
       async.parallel(tasks, function(err) { //This function gets called after the two tasks have called their "task callbacks"
           if (err) return next(err); //If an error occurred, let express handle it by calling the `next` function
           // Here `locals` will be an object with `users` and `colors` keys
           // Example: `locals = {users: [...], colors: [...]}`
           db.close();
-          res.render('profile/index', locals);
+          res.render('expense', {locals});
       });
+    })
+  
 // å
 
 
@@ -126,49 +180,49 @@ router.get('/expense', function(req, res, next) {
 // });
 
 //Immediate obligation expense CRUD
-router.post('/saveiexpense', function(req, res, next) {//all data is in req.body
-  console.log(req.body) //shows value in terminal
-  var IExpenseInfo = new iexpenseinfo({ //from top of the page, i. e variable name of model //new object instantiated
-    name : req.body.name,
-    amount : req.body.amount
-  }) 
-  var promise = IExpenseInfo.save()    //movie.save() returns promise so promise variable used only to represent
-  //await promise //if you use async function
-  promise.then((IExpenseInfo) => {//if you use normal promise
-    console.log('login info', IExpenseInfo)
-    res.redirect('/iexpense')
-  }).catch(err=> console.log(err+"could not save....."))
-});
+// router.post('/saveiexpense', function(req, res, next) {//all data is in req.body
+//   console.log(req.body) //shows value in terminal
+//   var IExpenseInfo = new iexpenseinfo({ //from top of the page, i. e variable name of model //new object instantiated
+//     name : req.body.name,
+//     amount : req.body.amount
+//   }) 
+//   var promise = IExpenseInfo.save()    //movie.save() returns promise so promise variable used only to represent
+//   //await promise //if you use async function
+//   promise.then((IExpenseInfo) => {//if you use normal promise
+//     console.log('login info', IExpenseInfo)
+//     res.redirect('/iexpense')
+//   }).catch(err=> console.log(err+"could not save....."))
+// });
 
-//True expenses CRUD
-router.post('/savetexpense', function(req, res, next) {//all data is in req.body
-  console.log(req.body) //shows value in terminal
-  var TExpenseInfo = new texpenseinfo({ //from top of the page, i. e variable name of model //new object instantiated
-    name : req.body.name,
-    amount : req.body.amount
-  }) 
-  var promise = TExpenseInfo.save()    //movie.save() returns promise so promise variable used only to represent
-  //await promise //if you use async function
-  promise.then((TExpenseInfo) => {//if you use normal promise
-    console.log('login info', TExpenseInfo)
-    res.redirect('/texpense')
-  }).catch(err=> console.log(err+"could not save....."))
-});
+// //True expenses CRUD
+// router.post('/savetexpense', function(req, res, next) {//all data is in req.body
+//   console.log(req.body) //shows value in terminal
+//   var TExpenseInfo = new texpenseinfo({ //from top of the page, i. e variable name of model //new object instantiated
+//     name : req.body.name,
+//     amount : req.body.amount
+//   }) 
+//   var promise = TExpenseInfo.save()    //movie.save() returns promise so promise variable used only to represent
+//   //await promise //if you use async function
+//   promise.then((TExpenseInfo) => {//if you use normal promise
+//     console.log('login info', TExpenseInfo)
+//     res.redirect('/texpense')
+//   }).catch(err=> console.log(err+"could not save....."))
+// });
 
-//True expenses CRUD
-router.post('/savepexpense', function(req, res, next) {//all data is in req.body
-  console.log(req.body) //shows value in terminal
-  var PExpenseInfo = new pexpenseinfo({ //from top of the page, i. e variable name of model //new object instantiated
-    name : req.body.name,
-    amount : req.body.amount
-  }) 
-  var promise = PExpenseInfo.save()    //movie.save() returns promise so promise variable used only to represent
-  //await promise //if you use async function
-  promise.then((PExpenseInfo) => {//if you use normal promise
-    console.log('login info', PExpenseInfo)
-    res.redirect('/pexpense')
-  }).catch(err=> console.log(err+"could not save....."))
-});
+// //True expenses CRUD
+// router.post('/savepexpense', function(req, res, next) {//all data is in req.body
+//   console.log(req.body) //shows value in terminal
+//   var PExpenseInfo = new pexpenseinfo({ //from top of the page, i. e variable name of model //new object instantiated
+//     name : req.body.name,
+//     amount : req.body.amount
+//   }) 
+//   var promise = PExpenseInfo.save()    //movie.save() returns promise so promise variable used only to represent
+//   //await promise //if you use async function
+//   promise.then((PExpenseInfo) => {//if you use normal promise
+//     console.log('login info', PExpenseInfo)
+//     res.redirect('/pexpense')
+//   }).catch(err=> console.log(err+"could not save....."))
+// });
 
 //goals CRUD
 
